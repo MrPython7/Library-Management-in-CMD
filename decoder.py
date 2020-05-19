71e730c6190b9ac1712043b49dbe6cab347a3b6c
myselfvishal7 <52602540+myselfvishal7@users.noreply.github.com>
2020-05-19 17:57:06 +0530

    Add files via upload

---
+++ decoder.py
@@ -0,0 +1,13 @@
+alphabet='abcdefghijklmnopqrstuvwxyz'
+key= 7
+
+newmessage=''
+
+message = input('Please enter a message:')
+
+for character in message:
+    if character in alphabet:
+        position= alphabet.find(character)
+        newposition=(position+ key)% 26
+        newcharacter= alphabet[newposition]
+        newmessage 
